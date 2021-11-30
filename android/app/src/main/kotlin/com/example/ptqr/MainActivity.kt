612e88118406ae7037387777f2d267e24aebff07
Arun kumaar Sreenivasan <arunkumaar.srinivasan@gmail.com>
2021-11-30 09:06:11 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/ptqr/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ptqr
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/writeData.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+// ignore: camel_case_types
+class writeData extends StatefulWidget {
+ writeData({Key? key}) : super(key: key);
+
+  @override
+   writeState createState() =>  writeState();
+}
+
+// ignore: camel_case_types
+class  writeState extends State <writeData> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
